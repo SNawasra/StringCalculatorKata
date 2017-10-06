ab2a6830231231bb5cacf87bda389df8f8602611
Sameh Nawasra <samehnawasra@hotmail.com>
2017-10-06 22:23:07 +0300

    adding an addition method for string contains two numbers at max, creating unit tests for string contains zero, one and two numbers :))

--- StringKataCalculator/StringKataCalculator.Tests/StringKataCalculatorTests.cs
+++ StringKataCalculator/StringKataCalculator.Tests/StringKataCalculatorTests.cs
@@ -9,10 +9,28 @@ namespace StringKataCalculator.Tests
 {
     public class StringKataCalculatorTests
     {
-        [Test]
-        public void IsTestRunning()
+        private StringKataCalculator _stringKataCalculator;
+        public StringKataCalculatorTests()
         {
+            _stringKataCalculator = new StringKataCalculator();
+        }
 
+        [Test]
+        public void Add_ShouldReturnZero_WhenStringNumbersIsEmpty()
+        {
+            Assert.AreEqual(0, _stringKataCalculator.Add(""));
+        }
+
+        [Test]
+        public void Add_ShouldReturnTheNumber_WhenStringNumbersContainsSingleNumber()
+        {
+            Assert.AreEqual(1, _stringKataCalculator.Add("1"));
+        }
+
+        [Test]
+        public void Add_ShouldReturnTheSumOfNumbers_WhenNumberStringContainsTwoNumbers()
+        {
+            Assert.AreEqual(3, _stringKataCalculator.Add("1,2,3"));
         }
     }
 }
--- StringKataCalculator/StringKataCalculator/StringKataCalculator.cs
+++ StringKataCalculator/StringKataCalculator/StringKataCalculator.cs
@@ -8,5 +8,33 @@ namespace StringKataCalculator
 {
     public class StringKataCalculator
     {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers)) return 0;
+            int sum = ParseStringNumbers(numbers);
+            return sum;
+        }
+
+        private int ParseStringNumbers(string numbers)
+        {
+            string[] numberList = numbers.Split(',');
+
+            if (numberList == null) return 0;
+            else if (numberList.Length == 0) return 0;
+            else if (numberList.Length == 1) 
+            {
+                int value = 0;
+                int.TryParse(numberList[0], out value);          
+                return value;
+            }
+            else
+            {
+                int value1 = 0;
+                int value2 = 0;
+                int.TryParse(numberList[0], out value1);
+                int.TryParse(numberList[1], out value2);
+                return value1 + value2;
+            }
+        }
     }
 }
